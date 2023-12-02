b70164a6aef0d0413245927c368b10d1e83b8605
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-02 11:51:54 +0000

    Added cli arg parsing, day_1 file
    - Run exercises by passing day number as cli argument
    - Added module "exercises" with utility fn run_day()
    - Added placeholder day_1.rs

---
+++ src/exercises/mod.rs
@@ -0,0 +1,8 @@
+pub mod day_1;
+
+pub fn run_day(day_number: u32) -> Result<(), &'static str> {
+    match day_number {
+        1 => Ok(day_1::run()),
+        _ => Err("Could not find exercise file. Day must be between 1 and 25."),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+mod exercises;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <day_number>", args[0]);
+        eprintln!("Please provide the day number as a command-line argument.");
+        std::process::exit(1);
+    }
+
+    let day_number: u32 = match args[1].parse() {
+        Ok(number) => number,
+        Err(_) => {
+            eprintln!("Error: could not parse day number. Must be an integer between 1 and 25.");
+            std::process::exit(1);
+        }
+    };
+
+    if let Err(e) = exercises::run_day(day_number) {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
