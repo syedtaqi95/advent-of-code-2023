8e8891d4e08b3367e6b4d582f8f22f400abd0c6d
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-11 22:39:39 +0000

    Added empty day 2

---
+++ src/exercises/day_2.rs
@@ -0,0 +1,4 @@
+pub fn run() -> Result<(), &'static str> {
+    println!("Running day 2...");
+    Ok(())
+}
--- src/exercises/mod.rs
+++ src/exercises/mod.rs
@@ -1,8 +1,10 @@
 pub mod day_1;
+pub mod day_2;
 
 pub fn run_day(day_number: u32) -> Result<(), &'static str> {
     match day_number {
         1 => day_1::run(),
+        2 => day_2::run(),
         _ => Err("Could not find exercise file. Day must be between 1 and 25."),
     }
 }
