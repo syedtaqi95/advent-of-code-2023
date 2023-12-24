52d7099f9683fb324beea46c58a239b86586a650
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-24 01:12:50 +0000

    Added consts

--- src/exercises/day_2.rs
+++ src/exercises/day_2.rs
@@ -1,12 +1,16 @@
 use crate::exercises::read_lines;
 
+const NUM_REDS: i32 = 12;
+const NUM_GREEN: i32 = 13;
+const NUM_BLUE: i32 = 14;
+
 pub fn run() -> Result<(), &'static str> {
     let file_path = "./src/exercises/day_2.data";
     match read_lines(file_path) {
         Ok(lines) => {
             for (i, line) in lines.enumerate() {
                 if let Ok(s) = line {
-                    println!("{}: {}\n", i, s);
+
                 }
             }
         }
